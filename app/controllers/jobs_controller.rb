3382d3ca30898976b2814e6adc11ea91f7d71845
AnattaGuo <mengjunkwok@gmail.com>
2017-07-06 21:17:55 +0800

    implement job CRUD and authentication

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,67 @@
+class JobsController < ApplicationController
+  before_action :set_job, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, except: [:index, :show]
+
+  # GET /jobs
+  # GET /jobs.json
+  def index
+    @jobs = Job.all
+  end
+
+  # GET /jobs/1
+  # GET /jobs/1.json
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  # GET /jobs/new
+  def new
+    @job = Job.new
+  end
+
+  # GET /jobs/1/edit
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  # POST /jobs
+  # POST /jobs.json
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /jobs/1
+  # PATCH/PUT /jobs/1.json
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to jobs_path
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /jobs/1
+  # DELETE /jobs/1.json
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    redirect_to jobs_paths
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_job
+      @job = Job.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def job_params
+      params.require(:job).permit(:title, :description)
+    end
+end
